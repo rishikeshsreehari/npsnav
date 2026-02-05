0b0e160c7c48edfbe26321e48de7dab134078fa2
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 23:13:18 +0800

    Improve fund segmentation logic in main.py

---
+++ src/funds_data.py
@@ -0,0 +1,6 @@
+# This file contains the fund data and should have the taxonomy updated
+funds = [
+    {"name": "SBI Pension Fund - Central Govt", "type": "Central Government"},
+    {"name": "HDFC Tax Saver Fund", "type": "Tax Saver"},
+    # Add more funds with their types as needed
+]
