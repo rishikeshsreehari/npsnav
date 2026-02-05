125398d147de02f250a63a5c55b00758a17f6dc1
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-06 00:02:10 +0800

    Improve scheme categorization logic in main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,19 @@
-import json
-
-def categorize_funds(funds):
+def categorize_schemes(schemes):
     categorized = {
-        "Tax Saver": [],
-        "Central Government": [],
-        "State Government": [],
-        "Others": []
+        'Tax Saver': [],
+        'Central Government': [],
+        'State Government': [],
+        'Others': []
     }
-    
-    for fund in funds:
-        if fund['type'] == 'Tax Saver':
-            categorized["Tax Saver"].append(fund)
-        elif fund['type'] == 'Central Government':
-            categorized["Central Government"].append(fund)
-        elif fund['type'] == 'State Government':
-            categorized["State Government"].append(fund)
+
+    for scheme in schemes:
+        if 'Tax Saver' in scheme.name:
+            categorized['Tax Saver'].append(scheme)
+        elif 'Central Government' in scheme.name:
+            categorized['Central Government'].append(scheme)
+        elif 'State Government' in scheme.name:
+            categorized['State Government'].append(scheme)
         else:
-            categorized["Others"].append(fund)
-    
+            categorized['Others'].append(scheme)
+
     return categorized
-
-def build_site():
-    with open('data/funds.json') as f:
-        funds = json.load(f)
-    
-    categorized_funds = categorize_funds(funds)
-    # Additional logic to build the site with categorized_funds
-
-if __name__ == "__main__":
-    build_site()
