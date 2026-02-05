eacacb2227a46d56906d11c2adea47b24b1adf86
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 23:11:15 +0800

    Fix fund filtering to correctly categorize NPS funds

--- src/main.py
+++ src/main.py
@@ -1,23 +1,19 @@
-def categorize_funds(fund):
-    if fund['type'] == 'Tax Saver':
-        return 'Tax Saver'
-    elif fund['type'] == 'Central Government':
-        return 'Central Government'
-    elif fund['type'] == 'State Government':
-        return 'State Government'
-    else:
-        return 'Others'
-
-def build_fund_categories(funds):
-    categories = {
+def categorize_funds(funds):
+    categorized_funds = {
         'Tax Saver': [],
         'Central Government': [],
         'State Government': [],
         'Others': []
     }
-    
+
     for fund in funds:
-        category = categorize_funds(fund)
-        categories[category].append(fund)
-    
-    return categories
+        if fund['type'] == 'Tax Saver':
+            categorized_funds['Tax Saver'].append(fund)
+        elif fund['type'] == 'Central Government':
+            categorized_funds['Central Government'].append(fund)
+        elif fund['type'] == 'State Government':
+            categorized_funds['State Government'].append(fund)
+        else:
+            categorized_funds['Others'].append(fund)
+
+    return categorized_funds
