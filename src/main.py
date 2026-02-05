99da0109877145676bb6d9e47a672fdc434bd7f1
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 23:02:04 +0800

    Improve fund segmentation on homepage for better categorization

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
-import json
+def segment_funds(funds):
+    """Segment funds into categories."""
+    categorized_funds = {
+        'Equity': [],
+        'Corporate Debt': [],
+        'Government Bonds': [],
+        'Tax Saver': [],
+        'Central Government': [],
+        'State Government': [],
+        'Others': []
+    }
 
-def load_funds():
-    with open('data/funding.json') as f:
-        return json.load(f)
-
-def categorize_fund(fund):
-    if "Tax Saver" in fund['name']:
-        return 'Tax Saver'
-    elif "Central Government" in fund['name']:
-        return 'Central Government'
-    elif "State Government" in fund['name']:
-        return 'State Government'
-    # Add more conditions here for better categorization
-    return 'Others'
-
-def build_fund_segments():
-    funds = load_funds()
-    segments = {'Tax Saver': [], 'Central Government': [], 'State Government': [], 'Others': []}
     for fund in funds:
-        category = categorize_fund(fund)
-        segments[category].append(fund)
-    return segments
+        if 'Equity' in fund['type']:
+            categorized_funds['Equity'].append(fund)
+        elif 'Corporate Debt' in fund['type']:
+            categorized_funds['Corporate Debt'].append(fund)
+        elif 'Government' in fund['type']:
+            if 'Central' in fund['type']:
+                categorized_funds['Central Government'].append(fund)
+            elif 'State' in fund['type']:
+                categorized_funds['State Government'].append(fund)
+            else:
+                categorized_funds['Government Bonds'].append(fund)
+        elif 'Tax Saver' in fund['type']:
+            categorized_funds['Tax Saver'].append(fund)
+        else:
+            categorized_funds['Others'].append(fund)
 
-if __name__ == "__main__":
-    segments = build_fund_segments()
-    # Code to display segments on the home page
+    return categorized_funds
