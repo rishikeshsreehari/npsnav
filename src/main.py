b7756467a9a5cd10341589e641b1dc165e4fe40a
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-06 01:42:23 +0800

    Improve fund filters on home page for better categorization

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 def categorize_funds(funds):
     categorized = {
-        'Equity': [],
-        'Debt': [],
-        'Tax Saver': [],
-        'Central Government': [],
-        'State Government': [],
-        'Others': []
+        "Equity": [],
+        "Corporate Bond": [],
+        "Government Bond": [],
+        "Tax Saver": [],
+        "Central Government": [],
+        "State Government": [],
+        "Others": []
     }
 
     for fund in funds:
         if fund['type'] == 'Equity':
-            categorized['Equity'].append(fund)
-        elif fund['type'] == 'Debt':
-            categorized['Debt'].append(fund)
-        elif fund['type'] == 'Tax Saver':
-            categorized['Tax Saver'].append(fund)
-        elif fund['type'] == 'Central Government':
-            categorized['Central Government'].append(fund)
-        elif fund['type'] == 'State Government':
-            categorized['State Government'].append(fund)
+            categorized["Equity"].append(fund)
+        elif fund['type'] == 'Corporate Bond':
+            categorized["Corporate Bond"].append(fund)
+        elif fund['type'] == 'Government Bond':
+            categorized["Government Bond"].append(fund)
+        elif fund['category'] == 'Tax Saver':
+            categorized["Tax Saver"].append(fund)
+        elif fund['category'] == 'Central Government':
+            categorized["Central Government"].append(fund)
+        elif fund['category'] == 'State Government':
+            categorized["State Government"].append(fund)
         else:
-            categorized['Others'].append(fund)
+            categorized["Others"].append(fund)
 
     return categorized
