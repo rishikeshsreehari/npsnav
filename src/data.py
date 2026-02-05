e9c66e26af89ae93c875158977b585d708ab93c8
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 23:06:53 +0800

    Fix fund categorization on home page for better segmentation

---
+++ src/data.py
@@ -0,0 +1,8 @@
+def fetch_fund_types():
+    return [
+        {"type": "Tax Saver", "name": "NPS Tax Saver Fund"},
+        {"type": "Central Government", "name": "NPS Central Fund"},
+        {"type": "State Government", "name": "NPS State Fund"},
+        {"type": "Equity", "name": "NPS Equity Fund"},
+        # additional fund types can be added here
+    ]
