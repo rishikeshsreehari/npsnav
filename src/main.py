029b1f524a605ca50d0acacfbef8eac4a87097be
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 23:13:16 +0800

    Improve fund segmentation logic in main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
-def categorize_funds(funds):
-    categorized_funds = {
-        'Tax Saver': [],
-        'Central Government': [],
-        'State Government': [],
-        'Others': []
-    }
-
-    for fund in funds:
-        if fund['type'] == 'Tax Saver':
-            categorized_funds['Tax Saver'].append(fund)
-        elif fund['type'] == 'Central Government':
-            categorized_funds['Central Government'].append(fund)
-        elif fund['type'] == 'State Government':
-            categorized_funds['State Government'].append(fund)
-        else:
-            categorized_funds['Others'].append(fund)
-
-    return categorized_funds
+def categorize_fund(scheme):
+    if 'Tax Saver' in scheme:
+        return 'Tax Saver'
+    elif 'Central Government' in scheme:
+        return 'Central Government'
+    elif 'State Government' in scheme:
+        return 'State Government'
+    # Additional categories can be added here
+    else:
+        return 'Others'
