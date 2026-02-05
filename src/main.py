1b2de20cbe20e4877c611fd67f5be9d0b496ce19
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 23:27:23 +0800

    Improve fund categorization on home page

--- src/main.py
+++ src/main.py
@@ -1,19 +1,9 @@
-def categorize_funds(funds):
-    categorized = {
-        "Tax Saver": [],
-        "Central Government": [],
-        "State Government": [],
-        "Others": []
-    }
-
-    for fund in funds:
-        if fund['type'] == 'Tax Saver':
-            categorized["Tax Saver"].append(fund)
-        elif fund['type'] == 'Central Government':
-            categorized["Central Government"].append(fund)
-        elif fund['type'] == 'State Government':
-            categorized["State Government"].append(fund)
-        else:
-            categorized["Others"].append(fund)
-
-    return categorized
+def categorize_funds(fund):
+    if "Tax Saver" in fund.name:
+        return "Tax Saver"
+    elif "Central Government" in fund.name:
+        return "Central Government"
+    elif "State Government" in fund.name:
+        return "State Government"
+    # existing conditions...
+    return "Others"  # default category
