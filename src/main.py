f9aa5e23cff8db2ac97e73d1a8a8ff0e046e2d8c
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-06 01:08:56 +0800

    Enhance fund categorization logic in main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
-import json
-
 def categorize_funds(funds):
     categorized = {
+        'Equity': [],
+        'Debt': [],
         'Tax Saver': [],
         'Central Government': [],
         'State Government': [],
         'Others': []
     }
-    
+
     for fund in funds:
-        if fund['type'] == 'Tax Saver':
+        if fund['type'] == 'Equity':
+            categorized['Equity'].append(fund)
+        elif fund['type'] == 'Debt':
+            categorized['Debt'].append(fund)
+        elif fund['type'] == 'Tax Saver':
             categorized['Tax Saver'].append(fund)
         elif fund['type'] == 'Central Government':
             categorized['Central Government'].append(fund)
@@ -17,17 +21,5 @@ def categorize_funds(funds):
             categorized['State Government'].append(fund)
         else:
             categorized['Others'].append(fund)
-    
+
     return categorized
-
-def load_funds(filepath):
-    with open(filepath) as f:
-        return json.load(f)
-
-def main():
-    funds = load_funds('data/funds.json')
-    categorized_funds = categorize_funds(funds)
-    # More logic to display the categorized funds on the home page
-
-if __name__ == "__main__":
-    main()
