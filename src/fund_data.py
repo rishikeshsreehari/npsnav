62b4184417614d8f89cf614a98445897f5bd261a
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 22:46:09 +0800

    Improve fund categorization on home page

---
+++ src/fund_data.py
@@ -0,0 +1,11 @@
+# Existing imports and code
+
+def get_fund_data():
+    # Sample data structure
+    return [
+        {"name": "Fund A", "type": "Tax Saver"},
+        {"name": "Fund B", "type": "Central Government"},
+        {"name": "Fund C", "type": "State Government"},
+        {"name": "Fund D", "type": "Other"},
+        # Add more fund data as needed
+    ]
