10e1f0575adb693d2024ba9a91f73eb5aa0667a0
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-05 22:59:45 +0800

    Improve fund categorization on the homepage

--- src/main.py
+++ src/main.py
@@ -1,21 +1,27 @@
-# Updated version of main.py to improve filtering logic
+import json
 
-def categorize_funds(funds):
-    categorized_funds = {
-        "Tax Saver": [],
-        "Central Government": [],
-        "State Government": [],
-        "Others": []
-    }
-    
+def load_funds():
+    with open('data/funding.json') as f:
+        return json.load(f)
+
+def categorize_fund(fund):
+    if "Tax Saver" in fund['name']:
+        return 'Tax Saver'
+    elif "Central Government" in fund['name']:
+        return 'Central Government'
+    elif "State Government" in fund['name']:
+        return 'State Government'
+    # Add more conditions here for better categorization
+    return 'Others'
+
+def build_fund_segments():
+    funds = load_funds()
+    segments = {'Tax Saver': [], 'Central Government': [], 'State Government': [], 'Others': []}
     for fund in funds:
-        if fund['type'] == "Tax Saver":
-            categorized_funds["Tax Saver"].append(fund)
-        elif fund['type'] == "Central Government":
-            categorized_funds["Central Government"].append(fund)
-        elif fund['type'] == "State Government":
-            categorized_funds["State Government"].append(fund)
-        else:
-            categorized_funds["Others"].append(fund)
-    
-    return categorized_funds
+        category = categorize_fund(fund)
+        segments[category].append(fund)
+    return segments
+
+if __name__ == "__main__":
+    segments = build_fund_segments()
+    # Code to display segments on the home page
