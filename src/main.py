1929e4af968dfbad701b1783a5526313fd0180c9
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-06 04:17:34 +0800

    Fix fund categorization logic to improve filtering on homepage

--- src/main.py
+++ src/main.py
@@ -1,28 +1,11 @@
-def categorize_funds(funds):
-    categorized = {
-        "Equity": [],
-        "Corporate Bond": [],
-        "Government Bond": [],
-        "Tax Saver": [],
-        "Central Government": [],
-        "State Government": [],
-        "Others": []
-    }
-
-    for fund in funds:
-        if fund['type'] == 'Equity':
-            categorized["Equity"].append(fund)
-        elif fund['type'] == 'Corporate Bond':
-            categorized["Corporate Bond"].append(fund)
-        elif fund['type'] == 'Government Bond':
-            categorized["Government Bond"].append(fund)
-        elif fund['category'] == 'Tax Saver':
-            categorized["Tax Saver"].append(fund)
-        elif fund['category'] == 'Central Government':
-            categorized["Central Government"].append(fund)
-        elif fund['category'] == 'State Government':
-            categorized["State Government"].append(fund)
-        else:
-            categorized["Others"].append(fund)
-
-    return categorized
+def categorize_fund(scheme_name):
+    """Categorize funds based on scheme name."""
+    if 'Tax Saver' in scheme_name:
+        return 'Tax Saver'
+    elif 'Central Government' in scheme_name:
+        return 'Central Government'
+    elif 'State Government' in scheme_name:
+        return 'State Government'
+    # Add more conditions as needed
+    else:
+        return 'Others'
